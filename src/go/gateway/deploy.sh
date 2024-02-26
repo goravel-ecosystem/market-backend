05436d1efe744c45d23fe831074f1a9eddb8196c
Bowen <hwbrzzl@gmail.com>
2024-02-26 18:54:27 +0800

    Optimize deploy.yml

--- src/go/gateway/deploy.sh
+++ src/go/gateway/deploy.sh
@@ -1,15 +1,15 @@
 docker run -dit \
-  -p $HTTP_PORT:$HTTP_PORT \
-  -e APP_ENV=$APP_ENV \
-  -e APP_KEY=$APP_KEY \
+  -p $INPUT_HTTP_PORT:$INPUT_HTTP_PORT \
+  -e APP_ENV=$INPUT_APP_ENV \
+  -e APP_KEY=$INPUT_APP_KEY \
   -e APP_DEBUG=true \
   -e APP_HOST=0.0.0.0 \
-  -e APP_PORT=$HTTP_PORT \
-  -e GRPC_USER_HOST=$GRPC_USER_HOST \
-  -e GRPC_USER_PORT=$GRPC_USER_PORT \
+  -e APP_PORT=$INPUT_HTTP_PORT \
+  -e GRPC_USER_HOST=$INPUT_GRPC_USER_HOST \
+  -e GRPC_USER_PORT=$INPUT_GRPC_USER_PORT \
   -e GATEWAY_HOST=0.0.0.0 \
-  -e GATEWAY_PORT=$GATEWAY_PORT \
+  -e GATEWAY_PORT=$INPUT_GATEWAY_PORT \
   --network goravel-market \
-  --network-alias $NAME \
-  --name goravel-market-$NAME \
-  $IMAGE
+  --network-alias $INPUT_APP_NAME \
+  --name goravel-market-$INPUT_APP_NAME \
+  $INPUT_IMAGE
