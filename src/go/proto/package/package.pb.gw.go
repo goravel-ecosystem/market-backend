16b7568e17656d19327f3261efe6f2506dd26acf
kkumar-gcc <krishkumar9352@gmail.com>
2024-04-05 15:52:36 +0530

    implement proto for get tags

--- src/go/proto/package/package.pb.gw.go
+++ src/go/proto/package/package.pb.gw.go
@@ -35,11 +35,7 @@ func request_PackageService_GetPackage_0(ctx context.Context, marshaler runtime.
 	var protoReq GetPackageRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -69,11 +65,7 @@ func local_request_PackageService_GetPackage_0(ctx context.Context, marshaler ru
 	var protoReq GetPackageRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
